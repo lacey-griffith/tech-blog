00b324047fba4feaca084bc5251071adfee9d1d0
Lacey <lacey.griffith@g.austincc.edu>
2021-05-22 15:42:21 -0500

    display blog posts on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,32 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {BlogPost, User, Comment} = require('../models')
+
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+BlogPost.findAll({
+    attributes: ['id', 'title', 'post_text', 'created_at'],
+    order: [['created_at', 'DESC']],
+    include: [
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id','user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
+    ]
+    })
+    .then(postData => {
+        const posts = postData.map(post => post.get({ plain: true }))
+        res.render('homepage', { posts })
+    })
+    .catch(err => res.status(500).json(err))
 });
 
 module.exports = router;
