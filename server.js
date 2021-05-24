a7578d39c21d5673d7b8aa1d06ebcfb8450d4456
Lacey <lacey.griffith@g.austincc.edu>
2021-05-23 19:07:39 -0500

    add date/word formatting, add comment functionality when logged in

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 //add session storing
 const session = require('express-session');
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,11 @@
 module.exports = {
-    //test functions
+    dateFormat: date => {
+        return `${new Date(date).getMonth()+1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`
+    },
+    wordFormat: (word, amount) => {
+        if(amount !== 1){
+            return `${word}s`;
+        }
+        return word;
+    }
 }
